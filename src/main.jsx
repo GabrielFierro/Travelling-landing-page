06d6e26e99f565a073c569683052eb424c582766
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-05-23 20:18:12 -0300

    add: the customized theme to the chakraProvider component

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // 2. Wrap ChakraProvider at the root of your app
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
