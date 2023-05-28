c07551735ba429b1c19a4877ab9f413b899e2172
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-05-28 18:53:03 -0300

    fix: change the navbar for the header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css';
 // Import components
-import Navbar from './components/navbar/Navbar';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Header />
     </>
   );
 }
