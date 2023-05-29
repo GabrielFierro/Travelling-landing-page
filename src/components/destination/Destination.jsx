5e629f858eb1c461c0cad08e22f16763b681bc59
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-05-29 18:23:05 -0300

    add: the text and the title with its styles

--- src/components/destination/Destination.jsx
+++ src/components/destination/Destination.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Box, Flex, Heading, Text } from '@chakra-ui/react';
 
 export default function Destination() {
-  return <div>Destination</div>;
+  return (
+    <Box maxW='100%' h='100vh' p={4}>
+      <Flex h='150px' direction='column' align='center' justify='center'>
+        <Text textStyle='h3' color='brand.accent' my='8'>
+          EXPLORE YOUR DREAM PLACE
+        </Text>
+        <Heading textStyle='h1' color='brand.black'>
+          Find Your Dream Destination
+        </Heading>
+      </Flex>
+    </Box>
+  );
 }
