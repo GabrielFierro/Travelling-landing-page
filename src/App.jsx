b812b47700d3547c15c2ddbccb6bef8c968da6ca
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-05-30 17:18:03 -0300

    add: the About component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 // Import components
 import Header from './components/header/Header';
 import Destination from './components/destination/Destination';
+import AboutUs from './components/about/About';
 
 function App() {
   return (
     <>
       <Header />
       <Destination />
+      <AboutUs />
     </>
   );
 }
