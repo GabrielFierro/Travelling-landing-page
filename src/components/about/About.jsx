6cf1c82e92ac980e280ab2d8fd3def88f95bc2b7
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-05-30 18:39:42 -0300

    add: the card image, with a description in anothe column

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
-import { Box, Flex, Heading } from '@chakra-ui/react';
+import {
+  Box,
+  Card,
+  Flex,
+  Heading,
+  Image,
+  Spacer,
+  Text
+} from '@chakra-ui/react';
+import Mountains from '../../assets/images/mountains.webp';
 
 export default function About() {
   return (
@@ -16,6 +25,54 @@ export default function About() {
             ABOUT <span>US</span>
           </Heading>
         </Flex>
+        <Box width='100%' h='430px' mt='12'>
+          <Flex px='12' mt='8' justify='space-evenly'>
+            <Card maxH={{ base: '100%', sm: '100px' }} maxW='md'>
+              <Image rounded='md' src={Mountains} alt='Mountains'></Image>
+            </Card>
+            <Spacer />
+            <Box maxW='xl' px='4'>
+              <Text textStyle='h2' color='brand.white'>
+                We provide all necessary details for tourists who are interested
+                in visiting beautiful destinations.
+              </Text>
+              <Flex justify='space-around' mt='9'>
+                <Box>
+                  <Text textStyle='h3' color='brand.accent'>
+                    12K
+                  </Text>
+                </Box>
+                <Box>
+                  <Text textStyle='h3' color='brand.accent'>
+                    16+
+                  </Text>
+                </Box>
+                <Box>
+                  <Text textStyle='h3' color='brand.accent'>
+                    10+
+                  </Text>
+                </Box>
+              </Flex>
+              <Flex justify='space-around' mt='3'>
+                <Box>
+                  <Text textStyle='navbar' color='brand.white'>
+                    Success Journey
+                  </Text>
+                </Box>
+                <Box>
+                  <Text textStyle='navbar' color='brand.white'>
+                    Awards Winning
+                  </Text>
+                </Box>
+                <Box>
+                  <Text textStyle='navbar' color='brand.white'>
+                    Years of Experience
+                  </Text>
+                </Box>
+              </Flex>
+            </Box>
+          </Flex>
+        </Box>
       </Box>
     </Box>
   );
