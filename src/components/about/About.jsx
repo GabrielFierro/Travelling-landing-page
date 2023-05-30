68a44e4b11d13214ddaac91a7f839a9ba9ea42fe
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-05-30 17:15:04 -0300

    add: styles for the title and add a background image

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { Box, Flex, Heading } from '@chakra-ui/react';
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <Box maxW='100%' h='100vh'>
+      <Box className='overlay overlay_2'>
+        <Flex align='center' justify='center'>
+          <Heading
+            className='about_us'
+            color='brand.white'
+            as='h1'
+            size='2xl'
+            mt='8'
+          >
+            ABOUT <span>US</span>
+          </Heading>
+        </Flex>
+      </Box>
+    </Box>
+  );
 }
