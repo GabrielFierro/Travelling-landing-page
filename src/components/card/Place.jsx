88458c1caf1ca7651d7418e3557cdb6e70b9f0e4
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-05-30 16:39:32 -0300

    add: the Place component who represent the card with image, location, and a description

---
+++ src/components/card/Place.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  Card,
+  CardBody,
+  Heading,
+  Flex,
+  Image,
+  Stack,
+  Text
+} from '@chakra-ui/react';
+// Import icons
+import LocationIcon from '../../assets/images/location-icon.svg';
+
+export default function Place({ src, alt, description }) {
+  return (
+    <Card maxW='sm'>
+      <CardBody>
+        <Image src={src} alt={alt} borderRadius='lg' />
+        <Stack mt='4' spacing='3'>
+          <Flex direction='row' justify='space-between'>
+            <Flex>
+              <Image src={LocationIcon} />
+              <Heading size='base'>{alt}</Heading>
+            </Flex>
+            <Text color='brand.accent' fontSize='md'>
+              $45.50
+            </Text>
+          </Flex>
+          <Text>{description}</Text>
+        </Stack>
+      </CardBody>
+    </Card>
+  );
+}
