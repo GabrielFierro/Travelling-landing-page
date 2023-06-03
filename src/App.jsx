2d45203c8bbae30d366cb2686b1497470ce15a39
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-06-03 17:52:26 -0300

    add: comment the import for the about us and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css';
 // Import components
 import Header from './components/header/Header';
-import AboutUs from './components/about/About';
-import Footer from './components/footer/Footer';
+// import AboutUs from './components/about/About';
+// import Footer from './components/footer/Footer';
 import Category from './components/destination/Category';
 
 function App() {
