4c0418838b2771fc2a9da68db4d2e5a81fb00a49
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-06-03 17:20:23 -0300

    add: display the titles, buttons, and the images

---
+++ src/components/destination/Category.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { Box, Flex, Heading, Text } from '@chakra-ui/react';
+// import the data about the images
+import { data } from './data';
+import Card from './Card';
+import ButtonHandle from './ButtonHandle';
+// Import components
+
+export default function Category() {
+  const [card, setCard] = useState(data);
+  const cats = ['All', ...new Set(data.map((card) => card.category))];
+
+  const filter = (cat) => {
+    if (cat === 'All') {
+      setCard(data);
+      return;
+    }
+    setCard(data.filter((item) => item.category === cat));
+  };
+
+  return (
+    <Box maxW='100%' h='500px'>
+      <Flex direction='column'>
+        <Flex h='300px' direction='column' align='center' justify='center'>
+          <Text textStyle='h3' color='brand.accent' my={3}>
+            EXPLORE YOUR DREAM PLACE
+          </Text>
+          <Heading color='brand.black'>Find Your Dream Destination</Heading>
+          <Flex maxW='800px' h='100px' direction='row'>
+            <ButtonHandle categories={cats} handleClick={filter} />
+          </Flex>
+        </Flex>
+        <Card allCards={card} />
+      </Flex>
+    </Box>
+  );
+}
