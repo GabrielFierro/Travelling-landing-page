b367b1c85169e9f7dc07e9f03592680dc899c30e
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-06-03 17:15:28 -0300

    add: an object data with properties for each image for the category component

---
+++ src/components/destination/data.jsx
@@ -0,0 +1,52 @@
+// Import images
+import MachuPichu from '../../assets/images/peru_machu_pichu.webp';
+import Italy from '../../assets/images/italy_venezia.webp';
+import Tokyo from '../../assets/images/japan_tokyo.webp';
+import FloridaImage from '../../assets/images/florida_miami.webp';
+import Alaska from '../../assets/images/alaska.webp';
+import SidneyAustralia from '../../assets/images/sydney_australia.webp';
+
+export const data = [
+  {
+    id: 1,
+    img: MachuPichu,
+    category: 'Mountain',
+    title: 'Machu Pichu, Peru',
+    rating: 8.6
+  },
+  {
+    id: 2,
+    img: Italy,
+    category: 'Recommend',
+    title: 'Venecia, Italy',
+    rating: 7.9
+  },
+  {
+    id: 3,
+    img: Tokyo,
+    category: 'Nature',
+    title: 'Tokyo, Japan',
+    rating: 9
+  },
+  {
+    id: 4,
+    img: FloridaImage,
+    category: 'Beach',
+    title: 'Florida, Miami',
+    rating: 8.6
+  },
+  {
+    id: 5,
+    img: Alaska,
+    category: 'Mountain',
+    title: 'Alaska, Canada',
+    rating: 8.5
+  },
+  {
+    id: 6,
+    img: SidneyAustralia,
+    category: 'Recommend',
+    title: 'Sidney, Australia',
+    rating: 8.2
+  }
+];
