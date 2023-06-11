0e30c320b8030278b2a5c4ca4295c9bf6bfe0fff
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-06-11 20:27:21 -0300

    add: the burger component

---
+++ src/components/navbar/BurguerButton.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function BurguerButton(props) {
+  return (
+    <div className='hamburger' onClick={props.handleClick}>
+      {/* hamburger icon, open the menu */}
+      {props.clicked ? (
+        <svg width='20' height='20' xmlns='http://www.w3.org/2000/svg'>
+          <path
+            d='M17.778.808l1.414 1.414L11.414 10l7.778 7.778-1.414 1.414L10 11.414l-7.778 7.778-1.414-1.414L8.586 10 .808 2.222 2.222.808 10 8.586 17.778.808z'
+            fill='#FFF'
+            fillRule='evenodd'
+          />
+        </svg>
+      ) : (
+        <svg width='24' height='16' xmlns='http://www.w3.org/2000/svg'>
+          <g fill='#FFF' fillRule='evenodd'>
+            <path d='M0 0h24v2H0zM0 7h24v2H0zM0 14h24v2H0z' />
+          </g>
+        </svg>
+      )}
+    </div>
+  );
+}
