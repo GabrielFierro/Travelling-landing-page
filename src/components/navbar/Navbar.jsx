3b6aa6d7742f444f0a1e401b4a761fcff41a453e
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-06-11 20:30:26 -0300

    add: properties, a useState hook and the Burger component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,61 +1,79 @@
-import {
-  Box,
-  Button,
-  ButtonGroup,
-  Flex,
-  Image,
-  Link,
-  Spacer
-} from '@chakra-ui/react';
-import React from 'react';
+import { Box, Button, ButtonGroup, Flex, Image, Link } from '@chakra-ui/react';
+import React, { useState } from 'react';
 import logo from '../../assets/images/logo.svg';
+import BurguerButton from './BurguerButton';
 
 export default function Navbar() {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
   return (
-    <Flex bg='bg.transparent' pos='absolute' zIndex={2} w='100%'>
-      <Box pt='6' pl='4'>
-        <Image src={logo} alt='Logo' cursor='pointer' />
-      </Box>
-      <Spacer />
-      <Flex>
+    <Flex bg='transparent' pos='absolute' zIndex={2} w='100%'>
+      <div className={`links ${clicked ? 'active' : ''}`}>
         <Flex
-          w='400px'
-          justify='space-around'
-          align='center'
-          color='brand.white'
-          textStyle='navbar'
+          direction={['column', 'row']}
+          justify={['space-between', 'center']}
+          w='100%'
+          h={['80px', '100%']}
         >
-          <Link href='#'>Home</Link>
-          <Link href='#'>Destination</Link>
-          <Link href='#'>About</Link>
-          <Link href='#'>Blog</Link>
+          <Box maxW='200px' pt='6' pl='4' h='100%'>
+            <Image w='200px' src={logo} alt='Logo' cursor='pointer' />
+          </Box>
+          <Flex
+            w='100%'
+            justify='center'
+            align={['center', 'flex-start']}
+            className='a'
+          >
+            <Flex
+              h={['550px', '80px']}
+              w='400px'
+              direction={['column', 'column', 'row']}
+              justify={['space-evenly', 'space-between', 'space-around']}
+              align='center'
+              color='brand.white'
+              textStyle='navbar'
+            >
+              <Link href='#'>Home</Link>
+              <Link href='#'>Destination</Link>
+              <Link href='#'>About</Link>
+              <Link href='#'>Blog</Link>
+            </Flex>
+          </Flex>
+          <ButtonGroup p='4' gap='2'>
+            <Button
+              w='100%'
+              h='50px'
+              color='brand.white'
+              variant='link'
+              px='4'
+              _hover={{
+                background: 'brand.white',
+                color: 'brand.black',
+                borderRadius: '2xl',
+                variant: 'solid'
+              }}
+            >
+              Login
+            </Button>
+            <Button
+              w='100%'
+              h='50px'
+              color='brand.black'
+              colorScheme='gray'
+              borderRadius='2xl'
+              variant='solid'
+              px='6'
+            >
+              Register
+            </Button>
+          </ButtonGroup>
         </Flex>
-      </Flex>
-      <Spacer />
-      <ButtonGroup p='4' gap='2'>
-        <Button
-          color='brand.white'
-          variant='link'
-          px='4'
-          _hover={{
-            background: 'brand.white',
-            color: 'brand.black',
-            borderRadius: '2xl',
-            variant: 'solid'
-          }}
-        >
-          Login
-        </Button>
-        <Button
-          color='brand.black'
-          colorScheme='gray'
-          borderRadius='2xl'
-          variant='solid'
-          px='6'
-        >
-          Register
-        </Button>
-      </ButtonGroup>
+      </div>
+      <BurguerButton clicked={clicked} handleClick={handleClick} />
     </Flex>
   );
 }
