417df72ecefcb3212c850c0b88c5c9cfdb4c5f93
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-06-23 20:12:01 -0300

    fix: the height of the component and delete unused component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,61 +12,59 @@ import Mountains from '../../assets/images/mountains.webp';
 
 export default function About() {
   return (
-    <Box maxW='100%' h='100vh'>
-      <Box>
-        <Flex align='center' justify='center'>
-          <Heading color='brand.black' as='h1' size='2xl' my='9'>
-            ABOUT <span>US</span>
-          </Heading>
+    <Box maxW='100%' h='auto'>
+      <Flex align='center' justify='center'>
+        <Heading color='brand.black' as='h1' size='2xl' my='9'>
+          ABOUT <span>US</span>
+        </Heading>
+      </Flex>
+      <Box width='100%' h={{ base: '300px', xs: '430px' }} py='8'>
+        <Flex px='16' mt='8'>
+          <Card maxH={{ base: '100%', sm: '100px' }} maxW='md'>
+            <Image rounded='md' src={Mountains} alt='Mountains'></Image>
+          </Card>
+          <Spacer />
+          <Box maxW='xl' px='4'>
+            <Text textStyle='h2' color='brand.black' align='center'>
+              We provide all necessary details for tourists who are interested
+              in visiting beautiful destinations.
+            </Text>
+            <Flex justify='space-around' mt='12'>
+              <Box>
+                <Text textStyle='h3' color='brand.accent'>
+                  12K
+                </Text>
+              </Box>
+              <Box>
+                <Text textStyle='h3' color='brand.accent'>
+                  16+
+                </Text>
+              </Box>
+              <Box>
+                <Text textStyle='h3' color='brand.accent'>
+                  10+
+                </Text>
+              </Box>
+            </Flex>
+            <Flex justify='space-around' mt='3'>
+              <Box w='120px' h='50px' align='center'>
+                <Text textStyle='navbar' color='brand.black'>
+                  Success Journey
+                </Text>
+              </Box>
+              <Box w='120px' h='50px' align='center'>
+                <Text textStyle='navbar' color='brand.black'>
+                  Awards Winning
+                </Text>
+              </Box>
+              <Box w='120px' h='50px' align='center'>
+                <Text textStyle='navbar' color='brand.black'>
+                  Years of Experience
+                </Text>
+              </Box>
+            </Flex>
+          </Box>
         </Flex>
-        <Box width='100%' h='430px' py='8'>
-          <Flex px='16' mt='8'>
-            <Card maxH={{ base: '100%', sm: '100px' }} maxW='md'>
-              <Image rounded='md' src={Mountains} alt='Mountains'></Image>
-            </Card>
-            <Spacer />
-            <Box maxW='xl' px='4'>
-              <Text textStyle='h2' color='brand.black' align='center'>
-                We provide all necessary details for tourists who are interested
-                in visiting beautiful destinations.
-              </Text>
-              <Flex justify='space-around' mt='12'>
-                <Box>
-                  <Text textStyle='h3' color='brand.accent'>
-                    12K
-                  </Text>
-                </Box>
-                <Box>
-                  <Text textStyle='h3' color='brand.accent'>
-                    16+
-                  </Text>
-                </Box>
-                <Box>
-                  <Text textStyle='h3' color='brand.accent'>
-                    10+
-                  </Text>
-                </Box>
-              </Flex>
-              <Flex justify='space-around' mt='3'>
-                <Box w='120px' h='50px' align='center'>
-                  <Text textStyle='navbar' color='brand.black'>
-                    Success Journey
-                  </Text>
-                </Box>
-                <Box w='120px' h='50px' align='center'>
-                  <Text textStyle='navbar' color='brand.black'>
-                    Awards Winning
-                  </Text>
-                </Box>
-                <Box w='120px' h='50px' align='center'>
-                  <Text textStyle='navbar' color='brand.black'>
-                    Years of Experience
-                  </Text>
-                </Box>
-              </Flex>
-            </Box>
-          </Flex>
-        </Box>
       </Box>
     </Box>
   );
