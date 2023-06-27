6e59c5da7c58ed4bd3d7da4430405f7a1f585e0d
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-06-27 17:21:19 -0300

    add: navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Header from './components/header/Header';
 import AboutUs from './components/about/About';
 import Footer from './components/footer/Footer';
 import Category from './components/destination/Category';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Header />
       <Category />
       <AboutUs />
