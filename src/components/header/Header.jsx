6970b03b5b26b76e91cd4490a384a5d38a5c2f8a
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-06-26 21:11:13 -0300

    fix: title size in all devices

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,13 @@ export default function Header() {
       <video autoPlay muted loop id='video'>
         <source src={video} type='video/mp4' />
       </video>
-      <Box pos='absolute' top='100' left='100' maxW='500px'>
+      <Box
+        pos='absolute'
+        top='100'
+        left={{ base: 70, md: 100 }}
+        maxW={{ base: '300px', md: '500px' }}
+        bg='tomato'
+      >
         <Text color='brand.white' textStyle='h1'>
           TRAVEL AROUND THE WORLD
         </Text>
