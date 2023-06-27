163fea0d229e814a4f0ea7fc19eb498fa56a35e5
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-06-27 16:41:16 -0300

    fix: move the description text to a new component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box, Card, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import { Box, Card, Flex, Heading, Image } from '@chakra-ui/react';
+import Description from './Description';
 import Mountains from '../../assets/images/mountains.webp';
 
 export default function About() {
@@ -27,60 +28,7 @@ export default function About() {
               alt='Mountains'
             ></Image>
           </Card>
-          <Flex
-            direction='column'
-            h='427px'
-            w={{ base: '100%', md: '700px' }}
-            px={16}
-            mt={12}
-          >
-            <Text
-              textStyle='h2'
-              fontSize={{ base: '24px', md: '34px' }}
-              color='brand.black'
-              align='center'
-              w='100%'
-              px={8}
-            >
-              We provide all necessary details for tourists who are interested
-              in visiting beautiful destinations.
-            </Text>
-            <Flex
-              justify='space-around'
-              align='center'
-              mt={16}
-              w='auto'
-              h='auto'
-              px={1}
-            >
-              <Text textStyle='h3' color='brand.accent'>
-                12K
-              </Text>
-              <Text textStyle='h3' color='brand.accent'>
-                16+
-              </Text>
-              <Text textStyle='h3' color='brand.accent'>
-                10+
-              </Text>
-            </Flex>
-            <Flex
-              justify='space-around'
-              mt='3'
-              w='100%'
-              h='auto'
-              textAlign='center'
-            >
-              <Text textStyle='navbar' color='brand.black' w='100px'>
-                Success Journey
-              </Text>
-              <Text textStyle='navbar' color='brand.black' w='100px'>
-                Awards Winning
-              </Text>
-              <Text textStyle='navbar' color='brand.black' w='100px'>
-                Years of Experience
-              </Text>
-            </Flex>
-          </Flex>
+          <Description />
         </Flex>
       </Box>
     </Box>
---
+++ src/components/about/Description.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Flex, Text } from '@chakra-ui/react';
+
+export default function Description() {
+  return (
+    <Flex
+      direction='column'
+      h='427px'
+      w={{ base: '100%', md: '700px' }}
+      px={16}
+      mt={12}
+    >
+      <Text
+        textStyle='h2'
+        fontSize={{ base: '24px', md: '34px' }}
+        color='brand.black'
+        align='center'
+        w='100%'
+        px={8}
+      >
+        We provide all necessary details for tourists who are interested in
+        visiting beautiful destinations.
+      </Text>
+      <Flex
+        justify='space-around'
+        align='center'
+        mt={16}
+        w='auto'
+        h='auto'
+        px={1}
+      >
+        <Text textStyle='h3' color='brand.accent'>
+          12K
+        </Text>
+        <Text textStyle='h3' color='brand.accent'>
+          16+
+        </Text>
+        <Text textStyle='h3' color='brand.accent'>
+          10+
+        </Text>
+      </Flex>
+      <Flex justify='space-around' mt='3' w='100%' h='auto' textAlign='center'>
+        <Text textStyle='navbar' color='brand.black' w='100px'>
+          Success Journey
+        </Text>
+        <Text textStyle='navbar' color='brand.black' w='100px'>
+          Awards Winning
+        </Text>
+        <Text textStyle='navbar' color='brand.black' w='100px'>
+          Years of Experience
+        </Text>
+      </Flex>
+    </Flex>
+  );
+}
