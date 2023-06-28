adfee791095dd49c8a6a5baae9528d3fb8959a23
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-06-28 18:37:03 -0300

    del: the import for the fonts

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,6 @@ import './index.css';
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from './theme.jsx';
-// Import the fonts
-// Rubik font
-import '@fontsource/rubik/400.css';
-import '@fontsource/rubik/500.css';
-import '@fontsource/rubik/700.css';
-// Montserrat font
-import '@fontsource/montserrat/500.css';
-import '@fontsource/montserrat/600.css';
-import '@fontsource/montserrat/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // 2. Wrap ChakraProvider at the root of your app
